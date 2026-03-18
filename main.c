64ca5397f033b15dbc34f49594e09ad2fca24946
student <student@codeby.local>
2026-03-18 03:15:22 +0100

    add static content support

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
   if (ev != MG_EV_HTTP_MSG) {
     return;
   }
-
+!!!!!!!!!!!!!!!!!!!!!!!!!
   struct mg_http_message *hm = (struct mg_http_message *) ev_data;
 
   if (mg_http_match_uri(hm, "/static/#")) {
