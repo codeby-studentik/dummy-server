1294608f4e4fd7c0c76cea3f71205bd8318116b6
student <student@codeby.local>
2026-03-18 03:05:21 +0100

    add json

--- main.c
+++ main.c
@@ -1,18 +1,3 @@
-// Copyright (c) Localhost Networks
-// All rights reserved
-//
-// This software is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License version 2 as
-// published by the Free Software Foundation. For the terms of this
-// license, see http://www.gnu.org/licenses/
-//
-// You are free to use this software under the terms of the GNU General
-// Public License, but WITHOUT ANY WARRANTY; without even the implied
-// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-// See the GNU General Public License for more details.
-//
-// SPDX-License-Identifier: GPL-2.0-only
-
 #include <stdlib.h>
 #include <string.h>
 #include "mongoose.h"
@@ -25,22 +10,9 @@ static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
   struct mg_http_message *hm = (struct mg_http_message *) ev_data;
 
   if (mg_http_match_uri(hm, "/static/#")) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    struct mg_http_serve_opts opts;
-    memset(&opts, 0, sizeof(opts));
-
-    opts.root_dir = ".";
-=======
     struct mg_http_serve_opts opts = {
       .root_dir = ".";
     };
->>>>>>> cd967c9237cb3cca7da9bede27ffbda7d0da2329
-=======
-    struct mg_http_serve_opts opts = {
-      .root_dir = ".";
-    };
->>>>>>> d66c601dee932390c0bf02aaf297e70b165c82ff
 
     mg_http_serve_dir(c, hm, &opts);
     return;
